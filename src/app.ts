de492496bf911b40c7198983f62512d8a93b013e
JonathanJoelVos <dev.jonathan.joel.vos@gmail.com>
2023-10-13 00:01:26 -0300

    fix: cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
 import fastify from "fastify"; 
 import { coffeeRoutes } from "./http/controllers/coffees/routes";
 import { labelsRoutes } from "./http/controllers/label/routes";
+import cors from '@fastify/cors'
+
 
 export const app = fastify()  
 
+app.register(cors, {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+})
 
 app.register(coffeeRoutes)
 app.register(labelsRoutes)
